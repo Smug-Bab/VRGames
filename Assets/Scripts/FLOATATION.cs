2a0a4e5a9902161ee0b316608278ead4bc44a3fa
Smug-Bab <myemailgotdestroyed@gmail.com>
2026-01-08 13:31:30 -0600

    .

--- Assets/Scripts/FLOATATION.cs
+++ Assets/Scripts/FLOATATION.cs
@@ -19,7 +19,6 @@ public class FLOATATION : MonoBehaviour
 
             var kart = BigP.transform.GetChild(1);
             var SmallP = BigP.transform.GetChild(0);
-            kart.forward = new Vector3(0, 1, 0);
 
             kart.transform.localPosition = Vector3.Lerp(Vector3.zero, kart.transform.localPosition, Time.deltaTime / 4);
             kart.transform.LookAt(player.transform.position);
